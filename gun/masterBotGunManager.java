fb706fcf28b70e5c1be4073aa5c8b4cd1a59e75c
Eugeniy Mikhailov <evgmik@gmail.com>
2015-09-30 21:54:12 -0400

    Bug fix: target tracking decison code moved to the proper place

--- gun/masterBotGunManager.java
+++ gun/masterBotGunManager.java
@@ -31,12 +31,12 @@ public class masterBotGunManager extends gunManager {
 	}
 
 	public void manage() {
+		if ( myBot.proxy.getGunHeat() < .5 ) {
+			myBot.getRadar().setNeedToTrackTarget( true );
+		} else {
+			myBot.getRadar().setNeedToTrackTarget( false );
+		}
 		if ( myBot.getTime() == fireAtTime &&  myBot.proxy.getGunTurnRemaining() == 0) {
-			if ( myBot.proxy.getGunHeat() < .5 ) {
-				myBot.getRadar().setNeedToTrackTarget( true );
-			} else {
-				myBot.getRadar().setNeedToTrackTarget( false );
-			}
 			// see firing pitfalls
 			// http://robowiki.net/wiki/Robocode/Game_Physics#Firing_Pitfall
 			// essentially we need to set firing solution at previous tick
