f8b4454f58d1706ca90e9d4b619a942780357814
Eugeniy Mikhailov <evgmik@gmail.com>
2015-10-03 17:44:14 -0400

    guessFactorGun: removed leftover from linear gun code, it is wrong here

--- gun/guessFactorGun.java
+++ gun/guessFactorGun.java
@@ -74,59 +74,14 @@ public class guessFactorGun extends baseGun {
 	}
 
 	public LinkedList<firingSolution> getFiringSolutions( InfoBot fBot, InfoBot tBot, long time, double bulletEnergy ) {
+		logger.error("getFiringSolutions should not be called like this: ( InfoBot fBot, InfoBot tBot, long time, double bulletEnergy )");
 		LinkedList<firingSolution> fSolultions = new LinkedList<firingSolution>();
-
-		botStatPoint fBStat = fBot.getStatClosestToTime( time );
-		if (fBStat == null)
-			return fSolultions;
-		Point2D.Double fPos = (Point2D.Double) fBStat.getPosition().clone();
-
-		fSolultions = getFiringSolutions( fPos, tBot, time, bulletEnergy);
-		fSolultions = setFiringBotName( fBot.getName(), fSolultions );
 		return fSolultions;
-
 	}
 
 	public LinkedList<firingSolution> getFiringSolutions( Point2D.Double fPos, InfoBot tBot, long time, double bulletEnergy ) {
+		logger.error("getFiringSolutions should not be called like this: ( Point2D.Double fPos, InfoBot tBot, long time, double bulletEnergy )");
 		LinkedList<firingSolution> fSolultions = new LinkedList<firingSolution>();
-		if (fPos == null )
-			return fSolultions;
-
-		// the latest time, when target stats are known, is at 'time-1'
-		botStatPoint tBStat = tBot.getStatClosestToTime( time - 1 );
-		if (tBStat == null)
-			return fSolultions;
-
-		// OK we know fire point and at least one target position
-		Point2D.Double tPos = (Point2D.Double) tBStat.getPosition().clone();
-		Point2D.Double vTvec = (Point2D.Double) tBStat.getVelocity().clone();
-
-		double bSpeed = physics.bulletSpeed( bulletEnergy );
-
-		// where target will be at wave intersection according to linear predictor
-		Point2D.Double tFPos = misc.linear_predictor( bSpeed, tPos, vTvec,  fPos);
-
-		firingSolution fS = new firingSolution( this, fPos, tFPos, time, bulletEnergy );
-
-		long infoLagTime = time - tBStat.getTime(); // ideally should be 0
-		if ( infoLagTime <= 0  ) {
-			// time point from the future
-			fS.setQualityOfSolution(1); // 1 is the best
-		}
-		if ( infoLagTime > 0  ) {
-			// we are using outdated info
-			fS.setQualityOfSolution( Math.exp(infoLagTime/5) );
-			logger.noise("time " + time + " target info is outdated by " + infoLagTime);
-		}
-		// check if the future target point is within botReacheable space
-		if ( !physics.botReacheableBattleField.contains( tFPos ) ) {
-			logger.noise("time " + time + " unphysical future target positkon");
-			fS.setQualityOfSolution( 0 ); // bad solution
-		}
-
-		logger.noise("linear gun firingSolution: " + fS.toString());
-		fSolultions.add(fS);
-		fSolultions = setTargetBotName( tBot.getName(), fSolultions );
 		return fSolultions;
 	}
 }
