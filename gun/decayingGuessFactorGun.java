7d3fa91aca47ad650091e7089f5857e958de90e0
Eugeniy Mikhailov <evgmik@gmail.com>
2015-10-04 00:37:47 -0400

    new gun: decayingGuessFactorGun

---
+++ gun/decayingGuessFactorGun.java
@@ -0,0 +1,25 @@
+// -*- java -*-
+
+package eem.frame.gun;
+import eem.frame.bot.*;
+import eem.frame.misc.*;
+
+import java.util.LinkedList;
+import java.awt.geom.Point2D;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+
+public class decayingGuessFactorGun extends guessFactorGun {
+	public decayingGuessFactorGun() {
+		gunName = "decayingGuessFactorGun";
+		color = new Color(0x00, 0xff, 0x00, 0xff);
+	}
+
+	@Override
+	protected double[] getRelevantGF( fighterBot fBot, InfoBot tBot ) {
+		return fBot.getGunManager().getDecayingGuessFactors( tBot.getName() );
+	}
+
+}
+
