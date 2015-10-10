eab4411e07fd72bf4f9e254c3013039a3fb3ccc8
Eugeniy Mikhailov <evgmik@gmail.com>
2015-10-09 21:14:41 -0400

    added flipLastGuessFactorGun

---
+++ gun/flipLastGuessFactorGun.java
@@ -0,0 +1,38 @@
+// -*- java -*-
+
+package eem.frame.gun;
+import eem.frame.bot.*;
+import eem.frame.misc.*;
+
+import java.util.LinkedList;
+import java.util.*;
+import java.awt.geom.Point2D;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+
+public class flipLastGuessFactorGun extends guessFactorGun {
+	public flipLastGuessFactorGun() {
+		gunName = "flipLastGuessFactorGun";
+		color = new Color(0xff, 0xff, 0x00, 0xff);
+	}
+
+	@Override
+	protected double[] getRelevantGF( fighterBot fBot, InfoBot tBot ) {
+		double[] lastGF = fBot.getGunManager().getDecayingGuessFactors( tBot.getName() );
+		double[] reversedGF = new double[ lastGF.length ];   
+		int left = 0;
+		int right = lastGF.length-1;
+		while (left <= right) {
+			// reversing array
+			reversedGF[ left  ] = lastGF[ right ];
+			reversedGF[ right ] = lastGF[ left  ];
+			left++;
+			right--;
+		}
+
+		return reversedGF;
+	}
+
+}
+
