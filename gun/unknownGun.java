d6e8fb101f7ed7ae8a19bc7712dcacb4bd492c77
Eugeniy Mikhailov <evgmik@gmail.com>
2015-10-16 00:01:39 -0400

    added unknownGun for statistical purposes

---
+++ gun/unknownGun.java
@@ -0,0 +1,19 @@
+// -*- java -*-
+
+package eem.frame.gun;
+import eem.frame.bot.*;
+import eem.frame.misc.*;
+
+import java.util.LinkedList;
+import java.awt.geom.Point2D;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+
+public class unknownGun extends baseGun {
+	public unknownGun() {
+		gunName = "unknownGun";
+		color = new Color(0x00, 0x00, 0x00, 0xff); // default color
+	}
+}
+
