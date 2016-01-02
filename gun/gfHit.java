aebd39c4378b38b83a6d3ff53ec03f562690b236
Eugeniy Mikhailov <evgmik@gmail.com>
2016-01-01 21:44:51 -0500

    added gfHit class to log entries to the guess factor storing trees

---
+++ gun/gfHit.java
@@ -0,0 +1,14 @@
+// -*- java -*-
+
+package eem.frame.gun;
+
+public class gfHit {
+	public int gfBin; 
+	public double weight;
+
+	public gfHit( int b, double w ) {
+		gfBin = b;
+		weight = w;
+	}
+}
+
