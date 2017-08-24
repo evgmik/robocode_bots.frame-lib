5cd3608c4959bc73e80e47a2bd6afb00f1ddbb98
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-08-24 00:19:48 -0400

    ArrayStats can do probability density and copies itself

--- misc/ArrayStats.java
+++ misc/ArrayStats.java
@@ -5,10 +5,16 @@ public class ArrayStats {
 	public double sum=0, sqSum=0, mean=0, std=0,
 	       max=Double.NEGATIVE_INFINITY, min=Double.POSITIVE_INFINITY;
        	public int indMax=0, indMin=0, length=0, nonZeroBinsN=0;
+	double[] bins;
 
 	public ArrayStats( double[] bins ) {
 		double b;
 		length = bins.length;
+		// copy input array
+		this.bins = new double[length];
+		for (int i=0; i < length; i++ ) {
+			this.bins[i] = bins[i];
+		}
 		for (int i=0; i < length; i++ ) {
 			b = bins[i];
 			sum += b; // calculates total count
@@ -31,5 +37,17 @@ public class ArrayStats {
 		} else {
 			logger.error( "ERROR: Do not send empty arrays to get its stats" );
 		}
+	}
+	
+	public double[] getProbDensity() {
+		//assume that array is all positive
+		double[] probDensity = new double[length];
+		double tot = sum;
+		if (tot == 0)
+		       tot = 1; // to avoid division by zero
+		for (int i=0; i < length; i++ ) {
+			probDensity[i] = bins[i]/tot;
+		}
+		return probDensity;
 	}
 }
