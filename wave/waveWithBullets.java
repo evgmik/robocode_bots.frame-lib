e2d8307a862ece82c43993ed6bb014bbef11e448
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-08-25 00:01:29 -0400

    modified bullets danger

--- wave/waveWithBullets.java
+++ wave/waveWithBullets.java
@@ -162,7 +162,7 @@ public class waveWithBullets extends wave {
 			// wave is passing through a bot at point dP
 			for ( firingSolution fS : firingSolutions ) {
 				//dL += fS.getDanger( time, dP );
-				dL += 4*fS.getDanger( time, botShadow );
+				dL += 1*fS.getDanger( time, botShadow );
 			}
 		}
 		profiler.stop("waveWithBullets.getFiringSolutionsDanger");
