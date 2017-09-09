21a237eb7bc8a07cfc44a4959472e7b019348eec
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-09-09 09:58:34 -0400

    gfHit now has gfCoverage parameter related to the hit bot shadow size

--- gun/gfHit.java
+++ gun/gfHit.java
@@ -3,12 +3,20 @@
 package eem.frame.gun;
 
 public class gfHit {
-	public int gfBin; 
-	public double weight;
+	public int gfBin; // which bin
+	public double weight; // weight of this gf, i.e. how much we trust it
+	public double gfCoverage; // the bot shadow, i.e how many nearby bins are affected
 
 	public gfHit( int b, double w ) {
 		gfBin = b;
 		weight = w;
+		gfCoverage = 1;
+	}
+
+	public gfHit( int b, double w, double gfC ) {
+		gfBin = b;
+		weight = w;
+		gfCoverage = gfC;
 	}
 }
 
