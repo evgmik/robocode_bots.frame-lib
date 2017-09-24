ef74085000f643c41853cc5170f807b87ceb74c5
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-09-23 22:48:34 -0400

    several new axilarly method to shorten path only to the wave hit point

--- dangermap/dangerPath.java
+++ dangermap/dangerPath.java
@@ -66,11 +66,69 @@ public class dangerPath implements Comparable<dangerPath> {
 		return dP;
 	}
 
+	public dangerPathPoint removeLast() {
+		dangerPathPoint dP = path.removeLast();
+		dangerLevel -= dP.getDanger();
+		return dP;
+	}
+
+
 	public dangerPathPoint getFirst() {
 		dangerPathPoint dP = path.getFirst();
 		return dP;
 	}
 
+	public dangerPathPoint getLast() {
+		dangerPathPoint dP = path.getLast();
+		return dP;
+	}
+
+	public void shortenToWaveHit() {
+		LinkedList<dangerPathPoint> shortPath = new LinkedList<dangerPathPoint>();
+		ListIterator<dangerPathPoint> iter = path.listIterator();
+		dangerPathPoint  dP;
+		int cnt = 0;
+		while (iter.hasNext()) {
+			cnt++;
+			dP = iter.next();
+			shortPath.add(dP);
+			if (dP.onTheWave)
+				break;
+		}
+		shortenTo( cnt );
+	}
+
+	public void shortenTo( int N ) { // make path N elements long
+		LinkedList<dangerPathPoint> shortPath = new LinkedList<dangerPathPoint>();
+		ListIterator<dangerPathPoint> iter = path.listIterator();
+		dangerPathPoint  dP;
+		int cnt = 0;
+		while (iter.hasNext()) {
+			cnt++;
+			dP = iter.next();
+			shortPath.add(dP);
+			if ( cnt >= N)
+				break;
+		}
+		path = shortPath;
+	}
+
+	public void truncateToSafestPoint() {
+		ListIterator<dangerPathPoint> iter = path.listIterator();
+		double smallestDanger = Double.POSITIVE_INFINITY;
+		dangerPathPoint  dP = null;
+		int cnt = 0;
+		int safestPointIndex=0;
+		while (iter.hasNext()) {
+			dP = iter.next();
+			if ( dP.getDanger() < smallestDanger ) {
+				smallestDanger = dP.getDanger();
+				safestPointIndex = cnt;
+			}
+			cnt++;
+		}
+		shortenTo( safestPointIndex + 1);
+	}
 
 	public int compare(dangerPath p1, dangerPath p2) {
 		double dL1 = p1.getDanger();
