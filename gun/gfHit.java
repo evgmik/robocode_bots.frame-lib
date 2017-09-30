db7f1c82f6cf89f4b34385347f056c9a0c2868b8
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-09-30 16:55:11 -0400

    gfHit keeps track when it was fired

--- gun/gfHit.java
+++ gun/gfHit.java
@@ -6,6 +6,7 @@ public class gfHit {
 	public int gfBin; // which bin
 	public double weight; // weight of this gf, i.e. how much we trust it
 	public double gfCoverage; // the bot shadow, i.e how many nearby bins are affected
+	public long   firedTime;  // time when the gun with this hit was fired
 
 	public gfHit( int b, double w ) {
 		gfBin = b;
@@ -13,10 +14,11 @@ public class gfHit {
 		gfCoverage = 1;
 	}
 
-	public gfHit( int b, double w, double gfC ) {
+	public gfHit( int b, double w, double gfC, long fT ) {
 		gfBin = b;
 		weight = w;
 		gfCoverage = gfC;
+		firedTime = fT;
 	}
 }
 
