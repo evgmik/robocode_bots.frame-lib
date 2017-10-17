441abd46a23a16f574a9e673492356efa4dab39b
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-10-16 23:22:33 -0400

    add bullets (if they calculated) to a virtual wave

--- gun/masterBotGunManager.java
+++ gun/masterBotGunManager.java
@@ -85,6 +85,14 @@ public class masterBotGunManager extends gunManager {
 				// fire virtual wave
 				wave nW = new wave( myBot.getInfoBot(), myBot.getTime(), bestFiringSolution.bulletEnergy );
 				myBot.getGameInfo()._wavesManager.add( nW );
+				if (firingSolutions.size() > 0) {
+					// since we have already calculated firing solutions
+					// let's add them to a wave
+					waveWithBullets wB = new waveWithBullets( nW, myBot.getGunManager().getGuessFactosrBinNum() );
+					wB.setTargetBot( targetBot );
+					wB.addFiringSolutions( firingSolutions );
+					myBot.myWaves.add(wB);
+				}
 			}
 		}
 	}
