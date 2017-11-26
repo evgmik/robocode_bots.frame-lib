3a53d0aee440e78dcb97a518d4df56bd5ef09d74
Eugeniy E. Mikhailov <evgmik@gmail.com>
2017-11-25 20:27:34 -0500

    removed redundant condition

--- gun/masterBotGunManager.java
+++ gun/masterBotGunManager.java
@@ -88,7 +88,7 @@ public class masterBotGunManager extends gunManager {
 					wave nW = new wave( myBot.getInfoBot(), myBot.getTime(), bestFiringSolution.bulletEnergy );
 					nW.realWave = false;
 					myBot.getGameInfo()._wavesManager.add( nW );
-					if (firingSolutions.size() > 0 && myBot.getTime() < physics.ticTimeFromTurnAndRound(200, 0) ) {
+					if (firingSolutions.size() > 0 ) {
 						// since we have already calculated firing solutions
 						// let's add them to a wave
 						HashMap<String, waveWithBullets> waveCache = new HashMap<String, waveWithBullets>();
